2f3d59c310816acda747e78f9b613a037ec21b49
root <root@ip-172-31-32-113.us-west-2.compute.internal>
2016-02-09 05:53:35 +0000

    Initialize strong loop API

--- common/models/account.js
+++ common/models/account.js
@@ -1,3 +1,14 @@
 module.exports = function(Account) {
-
+ Account.greet = function(cb) {
+    var msg = 'User';
+    cb(null, 'grettings.... ' + msg );
+ }
+ Account.remoteMethod(
+    'greet',
+    {
+        http: {path: '/greet', verb: 'get'},
+//        accepts: {arg: 'msg', type: 'string'},
+        returns: {arg: 'greeting', type: 'string'}
+    }
+ );
 };
