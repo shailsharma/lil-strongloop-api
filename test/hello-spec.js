c5a98b5d426bfc2a1359b4e676fd4b778f3089dc
Pradip Hudekar <phudekar@equalexperts.com>
2016-02-09 11:34:03 +0530

    added simple test

--- test/hello-spec.js
+++ test/hello-spec.js
@@ -1,11 +1,13 @@
-
+var describe = require('mocha').describe;
+var it = require('mocha').it;
 var expect = require('chai').expect;
 
-describe("Mocha tests", function(){
+describe('Mocha tests', function(){
 
-  it("should say hello", function(){
-    var message = "Hello";
-    expect(message).to.equal("Hello");
-  })
+  it('should say hello', function(){
+    var message = 'Hello';
+    expect(message).to.equal('Hello');
+  });
 
-})
+});
+>>>>>>> added simple test
