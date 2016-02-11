c7dbef0aecc50b27447cb22b4f364d391f8da1b2
tushargangwal <tushar.gangwal@gmail.com>
2016-02-11 17:33:33 +0530

    added test cases

---
+++ test/Account_test.js
@@ -0,0 +1,44 @@
+var request = require('request');
+var describe = require('mocha').describe;
+var it = require('mocha').it;
+var chai = require('chai');
+var should = chai.should();
+var expect = chai.expect;
+var chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+describe('Mocha tests', function(){
+
+  it('should say hello', function(){
+    var message = 'Hello Strong Loop';
+    expect(message).to.equal('Hello Strong Loop');
+  });
+
+});
+
+describe('Get /greet', function(){
+  it('Greet user', function(done){
+  	url = 'http://54.69.197.32:3000/api/accounts/greet';
+    request(url, function(error, response, body) {
+    	expect(response.statusCode).to.equal(200);
+    	expect(body).to.equal('{"results":" Greetings "}');
+    	console.log("Body:" + body);
+    	console.log("Response:" + response);
+    	done();
+    });
+  });
+});
+
+describe('Post /sendGreet', function(){
+  it('Send Greetings via Post', function(done){
+  	url = 'http://54.69.197.32:3000/api/accounts';
+    chai.request(url)
+    .post('/sendGreet')
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.be.json;
+      done();
+   });  
+  });
+});
