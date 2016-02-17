0300c591fcfee05dbbb63891f01a64be4e3ab820
tushargangwal <tushar.gangwal@gmail.com>
2016-02-17 19:09:11 +0530

    api documentation added

--- server/server.js
+++ server/server.js
@@ -1,7 +1,39 @@
 var loopback = require('loopback');
 var boot = require('loopback-boot');
-
 var app = module.exports = loopback();
+//
+var swaggerTools = require('swagger-tools');
+var jsyaml = require('js-yaml');
+var fs = require('fs');
+var options = {
+  swaggerUi: '/swagger.json',
+  controllers: './common/models',
+  useStubs: process.env.NODE_ENV === 'development' ? true : false // Conditionally turn on stubs (mock mode)
+};
+var spec = fs.readFileSync('./server/doc_swagger.yaml', 'utf8');
+var swaggerDoc = jsyaml.safeLoad(spec);
+//
+
+
+swaggerTools.initializeMiddleware(swaggerDoc, function (middleware) {
+  // Interpret Swagger resources and attach metadata to request - must be first in swagger-tools middleware chain
+  app.use(middleware.swaggerMetadata());
+
+  // Validate Swagger requests
+  app.use(middleware.swaggerValidator());
+
+  // Route validated requests to appropriate controller
+  app.use(middleware.swaggerRouter(options));
+
+  // Serve the Swagger documents and Swagger UI
+  app.use(middleware.swaggerUi());
+
+  // Start the server
+  // http.createServer(app).listen(serverPort, function () {
+  //   console.log('Your server is listening on port %d (http://localhost:%d)', serverPort, serverPort);
+  //   console.log('Swagger-ui is available on http://localhost:%d/docs', serverPort);
+  // });
+});
 
 app.start = function() {
   // start the web server
