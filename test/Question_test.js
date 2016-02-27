6f3c70107598c32580456016c378d144071c1e8d
Dindrawat <disha.indrawat@gmail.com>
2016-02-27 12:10:21 +0530

    Updated Question test case

--- test/Question_test.js
+++ test/Question_test.js
@@ -57,7 +57,27 @@ describe('CRUD Operations on Question Model', function(){
      });
    });
 
-   it('Deleting Question Data', function(done){
+  it('Updating Question Data', function(done){
+    url = 'http://localhost:3000/api/questions/';
+    chai.request(url)
+    .put(id)
+    .send({'order':3})
+    .end(function(err, res){
+      res.should.have.status(200);
+      res.should.have.status(200);
+      res.should.be.json;
+      console.log("Id:" + id)
+      console.log("Body" + res.body);
+      res.body.should.have.property('id');
+      res.body.should.have.property('order');
+      res.body.should.have.property('display');
+      res.body.order.should.equal(3);
+      res.body.display.should.equal("Multiple Choice");
+      done();
+     });
+   });
+
+  it('Deleting Question Data', function(done){
     url = 'http://localhost:3000/api/questions/';
     chai.request(url)
     .delete(id)
